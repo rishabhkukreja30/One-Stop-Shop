c7b9ab63a7b2f39ec17eee6387290fbceb25ad73
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-10-29 15:42:07 +0530

    generated jason web token to protect certain routes from user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,8 @@
 import Product from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'    
 import User from '../models/userModel.js'
+import generateTokens  from '../utils/generateTokens.js'
+import generateToken from '../utils/generateTokens.js'
 
 // @desc Auth the user and get token
 // @route POST /api/users/login
@@ -16,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }   else {
         res.status(401)
