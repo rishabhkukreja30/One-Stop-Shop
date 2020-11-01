afaf08c1c1c97e3a73a07526e663c3528e85f322
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-01 16:37:59 +0530

    admin screen access security

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import {USER_LOGIN_FAIL, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_REQUEST, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LIST_FAIL} from '../constants/userConstants'
+import {USER_LOGIN_FAIL, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_REQUEST, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LIST_FAIL, USER_LIST_RESET} from '../constants/userConstants'
 
 
   export const userLoginReducer = (state = {},   action) => {
@@ -67,6 +67,10 @@ export const userListReducer = (state = { users: []},   action) => {
       return { loading: false ,users: action.payload }
     case USER_LIST_FAIL:
       return { loading: false, error: action.payload }
+    case USER_LIST_RESET:
+      return {
+        users: []
+      }
     default:
       return state
   }
