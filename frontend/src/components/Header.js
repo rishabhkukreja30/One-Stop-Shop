52a0bee28efbe27aa2bc974cfe237cb4dbe5357c
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-02 15:47:20 +0530

    created admin productListScreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -47,10 +47,10 @@ const Header = () => {
                             <NavDropdown.Item>Users</NavDropdown.Item>
                         </LinkContainer>
                         
-                        <LinkContainer to='admin/productlist'>
+                        <LinkContainer to='/admin/productlist'>
                             <NavDropdown.Item>Products</NavDropdown.Item>
                         </LinkContainer>
-                        <LinkContainer to='admin/orderlist'>
+                        <LinkContainer to='/admin/orderlist'>
                             <NavDropdown.Item>Orders</NavDropdown.Item>
                         </LinkContainer>
                     </NavDropdown>
