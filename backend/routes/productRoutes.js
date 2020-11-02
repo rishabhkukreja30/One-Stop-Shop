4ae36fd3f01090541637db16bc4f14c9127a734c
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-03 01:10:11 +0530

    create review endpoint in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {getProducts, getProductById, deleteProduct, createProduct, updateProduct} from '../controllers/productControllers.js'
+import {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview} from '../controllers/productControllers.js'
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id').get(getProductById).delete(protect , admin , deleteProduct).put(protect, admin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 
 export default router 
