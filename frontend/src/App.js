709a7f3acd339a26d6483ae64b215e9a3dcb395c
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-03 17:51:46 +0530

    added pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
@@ -14,7 +14,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
-import  UserEditScreen  from './screens/UserEditScreen'
+import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
 import OrderListScreen from './screens/OrderListScreen'
@@ -22,30 +22,45 @@ import OrderListScreen from './screens/OrderListScreen'
 const App = () => {
   return (
     <Router>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <Route path='/login' component={LoginScreen}/>
-        <Route path='/register' component={RegisterScreen}/>
-        <Route path='/profile' component={ProfileScreen}/>
-        <Route path='/payment' component={PaymentScreen}/>
-        <Route path='/placeorder' component={PlaceOrderScreen}/>
-        <Route path='/shipping' component={ShippingScreen}/>
-        <Route path='/order/:id' component={OrderScreen}/>
-        <Route path='/product/:id' component={ProductScreen}/>
-        <Route path='/cart/:id?' component={CartScreen}/>
-        <Route path='/admin/userList' component={UserListScreen}/>
-        <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
-        <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
-        <Route path='/admin/productlist' component={ProductListScreen}/>
-        <Route path='/admin/orderlist' component={OrderListScreen}/>
-        <Route path='/search/:keyword' component={HomeScreen}/>
-        <Route path='/' component={HomeScreen} exact/>
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route
+            path='/admin/productlist'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+            exact
+          />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
     </Router>
   )
 }
 
-export default App;
+export default App
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,108 +1,139 @@
 import React, { useEffect } from 'react'
-import {LinkContainer} from 'react-router-bootstrap'
-import { Table , Button, Row, Col } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import  {listProducts, deleteProduct, createProduct}  from '../actions/productActions'
-import {PRODUCT_CREATE_RESET} from '../constants/productConstants'
+import Paginate from '../components/Paginate'
+import {
+  listProducts,
+  deleteProduct,
+  createProduct,
+} from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
-const ProductListScreen = ({match, history}) => {
+const ProductListScreen = ({ history, match }) => {
+  const pageNumber = match.params.pageNumber || 1
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const productList = useSelector(state => state.productList)
-    const {loading, error, products} = productList
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products, page, pages } = productList
 
-    const productDelete = useSelector(state => state.productDelete)
-    const {loading: loadingDelete , error: errorDelete, successDelete} = productDelete
+  const productDelete = useSelector((state) => state.productDelete)
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete
 
-    const productCreate = useSelector(state => state.productCreate)
-    const {loading: loadingCreate , error: errorCreate, success: successCreate, product: createdProduct} = productCreate
+  const productCreate = useSelector((state) => state.productCreate)
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-    const userLogin = useSelector(state => state.userLogin)
-    const {userInfo} = userLogin
+  useEffect(() => {
+    dispatch({ type: PRODUCT_CREATE_RESET })
 
-    useEffect(() => {
-
-        dispatch({type: PRODUCT_CREATE_RESET})
-
-        if(!userInfo.isAdmin ) {
-            history.push('/login')
-        }
-        
-        if(successCreate) {
-            history.push(`/admin/product/${createdProduct._id}/edit`)
-        }  else {
-            dispatch(listProducts())
-        }
-        
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
-
-    const deleteHandler = (id) => {
-        
-        if(window.confirm('Are you Sure ?')) {
-                dispatch(deleteProduct(id))    
-        }
+    if (!userInfo.isAdmin) {
+      history.push('/login')
     }
 
-    const createProductHandler = () => {
-        dispatch(createProduct())
+    if (successCreate) {
+      history.push(`/admin/product/${createdProduct._id}/edit`)
+    } else {
+      dispatch(listProducts('', pageNumber))
     }
+  }, [
+    dispatch,
+    history,
+    userInfo,
+    successDelete,
+    successCreate,
+    createdProduct,
+    pageNumber,
+  ])
 
-    return (
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+      dispatch(deleteProduct(id))
+    }
+  }
+
+  const createProductHandler = () => {
+    dispatch(createProduct())
+  }
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fas fa-plus'></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <>
-        <Row className='align-items-center'>
-            <Col>
-                <h1>Products</h1>
-            </Col>
-            <Col className='text-right'>
-                <Button className='my-3' onClick={createProductHandler}>
-                    <i className='fas fa-plus'></i> Create Product
-                </Button>
-            </Col>
-        </Row>
-        {loadingDelete && <Loader/>}
-    {errorDelete &&  <Message variant='danger'>{errorDelete}</Message>}
-    {loadingCreate && <Loader/>}
-    {errorCreate &&  <Message variant='danger'>{errorCreate}</Message>}
-         {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
-             <Table striped bordered responsive className='table-sm table-color'>
-                 <thead>
-                     <tr>
-                         <th>ID</th>
-                         <th>NAME</th>
-                         <th>PRICE</th>
-                         <th>CATEGORY</th>
-                         <th>BRAND</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     {products.map(product => (
-                         <tr key={product._id}>
-                             <td>{product._id}</td>
-                             <td>{product.name}</td>
-                             <td>₹{product.price}</td>
-                            <td>{product.category}</td>
-                            <td>{product.brand}</td>
-                             <td>
-                                 <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                     <Button variant='flush' className='btn-sm'>
-                                         <i className='fas fa-edit'></i>
-                                     </Button>
-                                 </LinkContainer>
-                                 <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
-                                     <i className='fas fa-trash'></i>
-                                 </Button>
-                             </td>
-                         </tr>
-                     ))}
-                 </tbody>
-             </Table>
-         ) }
+          <Table striped bordered responsive className='table-sm table-color'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant='light' className='btn-sm'>
+                        <i className='fas fa-edit'></i>
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant='danger'
+                      className='btn-sm'
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Paginate pages={pages} page={page} isAdmin={true} />
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default ProductListScreen
