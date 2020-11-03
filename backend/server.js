b7ee773f6ffc8a95e224b4837cd027b6e513a56c
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-04 00:38:07 +0530

    preparation to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -20,13 +21,22 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+const __dirname = path.resolve()
+if(process.env.NODE_ENV === 'production' ) {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html')))
+}  else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
+
 
 // error middleware
 app.use(notFound)
